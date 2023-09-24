4951fd0863a4fcd6fe59f8483cfaa634612fb440
Gonzalo Crudo <130198758+gonzacrudo28@users.noreply.github.com>
2023-09-24 11:14:50 -0300

    Update lista_enlazada.go
    Bases funciones del iterador interno

--- lista_enlazada.go
+++ lista_enlazada.go
@@ -37,7 +37,7 @@ func (lista *listaEnlazada[T]) InsertarPrimero(dato T) {
 
 }
 
-// Permite insertar un elemento en la ultima poscicio de la lista.
+// Permite insertar un elemento en la ultima poscicion de la lista.
 func (lista *listaEnlazada[T]) InsertarUltimo(dato T) {
 	nodo := nodoCrear[T](dato)
 	lista.ultimo.prox = nodo
@@ -46,11 +46,12 @@ func (lista *listaEnlazada[T]) InsertarUltimo(dato T) {
 	lista.largo++
 }
 
-// Se encarga de borra el primer elemento de la lista enlazada y devolverlo.
+// Se encarga de borrar el primer elemento de la lista enlazada y devolverlo.
 func (lista *listaEnlazada[T]) BorrarPrimero() T {
-	if lista.EstaVacia() {
-		panic("La lista esta vacia")
-	}
+	// if lista.EstaVacia() {
+	// 	panic("La lista esta vacia")
+	// }
+	lista.verSiEstaVacia()
 	dato := lista.primero.dato
 	lista.primero = lista.primero.prox
 	lista.largo--
@@ -59,17 +60,19 @@ func (lista *listaEnlazada[T]) BorrarPrimero() T {
 
 // Permite mostrar el dato del primer elemento de la lista.
 func (lista *listaEnlazada[T]) VerPrimero() T {
-	if lista.EstaVacia() {
-		panic("La lista esta vacia")
-	}
+	// if lista.EstaVacia() {
+	// 	panic("La lista esta vacia")
+	// }
+	lista.verSiEstaVacia()
 	return lista.primero.dato
 }
 
 // Permite mostrar el dato del ultimo elemento de la lista.
 func (lista *listaEnlazada[T]) VerUltimo() T {
-	if lista.EstaVacia() {
-		panic("La lista esta vacia")
-	}
+	// if lista.EstaVacia() {
+	// 	panic("La lista esta vacia")
+	// }
+	lista.verSiEstaVacia()
 	return lista.ultimo.dato
 }
 
@@ -78,14 +81,31 @@ func (lista *listaEnlazada[T]) Largo() int {
 	return lista.largo
 }
 
+func (lista *listaEnlazada[T]) verSiEstaVacia() {
+	// Esta funcion verifica si la lista esta vacia, si lo esta, entra en panico
+	if lista.EstaVacia() {
+		panic("La lista esta vacia")
+	}
+}
+
 // Iterador Lista
 
 // Permite aplicar la funcion a todos los elementos de la lista, teniendo dos condiciones de corte, que no haya mas elementos en la lista o que se devuelva falso.
 func (lista *listaEnlazada[T]) Iterar(visitar func(T) bool) {
-	for iter := lista.Iterador(); iter.HaySiguiente(); iter.Siguiente() {
-		//	visitar(iter)
+	// for iter := lista.Iterador(); iter.HaySiguiente(); iter.Siguiente() {
+
+	// }
+	// La verdad que no me acuerdo muy bien como usar ese ciclo for (me acuerdo que a Martin le gustaba mas pero yo creo que asi podria andar)
+	iter := lista.Iterador()
+	for iter.HaySiguiente() {
+		valor := iter.Siguiente()
+		// no me acuerdo si habia que cortar cuando sea true o cuando sea false, en este caso lo puse por si es false-> termino la iteracion
+		if !visitar(valor) {
+			break
+		}
+
 	}
-} // NO SE COMO HACER ESTO
+}
 
 type iterListaEnlazada[T any] struct {
 	// Necesitamos el actual, anterior para cuando insertemos o borremos no perdamos las referencias
@@ -97,9 +117,51 @@ type iterListaEnlazada[T any] struct {
 // Devuelve un iterador propio de la lista.
 func (lista *listaEnlazada[T]) Iterador() IteradorLista[T] {
 	iterador := iterListaEnlazada[T]{
-		iterador.lista:    lista,
-		iterador.actual:   lista.primero,
-		iterador.anterior: lista.primero,
+		// iterador.lista:    lista,
+		// iterador.actual:   lista.primero,
+		// iterador.anterior: lista.primero,
+		// ¿No seria asi en realidad?:
+		actual:   lista.primero,
+		anterior: lista.primero,
+		lista:    lista,
 	}
 	return &iterador
+}
+func (lista *listaEnlazada[T]) VerActual() T {
+	if iter.actual == nil {
+		panic("El iterador termino de iterar")
+	} else {
+		return iter.actual.dato
+	}
+}
+func (lista *listaEnlazada[T]) HaySiguiente() bool {
+	return iter.actual.prox != nil
+}
+func (lista *listaEnlazada[T]) Siguiente() {
+	if iter.actual.prox == nil {
+		panic("El iterador termino de iterar")
+	} else {
+		iter.actual = iter.actual.prox
+		iter.anterior = iter.anterior.prox
+	}
+}
+func (lista *listaEnlazada[T]) Insertar(elemento T) {
+	// Esta me tosqueó
+}
+func (lista *listaEnlazada[T]) Borrar() T {
+	// Esta funcion medio que me dejo en conflicto, la voy a hacer poniendo panics si se hacen operaciones invalidas, te pido que le pegues una chequeada y hagas los cambios que consideres necesarios xxq dudo q esta bien de una, pero creo que es la base :$
+	if iter.actual == nil {
+		panic("El iterador termino de iterar")
+	}
+	//Si mi elemento es el primero
+	elemento = iter.actual.dato
+	if iter.actual == iter.anterior {
+		// Hago que el que era el primero sea el segundo
+		iter.actual = iter.actual.prox
+	} else {
+		iter.anterior.prox = iter.actual.prox
+	}
+	iter.actual = iter.actual.prox
+	iter.anterior = iter.anterior.prox
+	return elemento
 }
