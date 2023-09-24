f662f36b0450d167a11db9f81cc570e9dd8f0fa1
NicolasChaia <131036051+NicolasChaia@users.noreply.github.com>
2023-09-24 11:13:27 -0300

    Update lista.go

--- lista.go
+++ lista.go
@@ -1,6 +1,5 @@
 package lista
 //Archivo con las primitivas
-// dgnlksdws
 type Lista[T any] interface {
 	// Corrobora si una lista esta vacia, devolviendo un dato del tipo booleano en tal caso.
 	EstaVacia() bool
