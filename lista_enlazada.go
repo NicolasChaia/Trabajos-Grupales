23d7037d7440863fb0c1e6e1b7183a12b3a350d8
Gonzalo Crudo <130198758+gonzacrudo28@users.noreply.github.com>
2023-10-03 20:14:53 -0300

    Update lista_enlazada.go

--- lista_enlazada.go
+++ lista_enlazada.go
@@ -14,6 +14,7 @@ func nodoCrear[T any](dato T) *nodoLista[T] {
 	nodo := nodoLista[T]{dato: dato, prox: nil}
 	return &nodo
 }
+
 func CrearListaEnlazada[T any]() Lista[T] {
 	lista := listaEnlazada[T]{
 		primero: nil,
@@ -24,27 +25,27 @@ func CrearListaEnlazada[T any]() Lista[T] {
 }
 
 func (lista *listaEnlazada[T]) EstaVacia() bool {
-	return lista.primero == nil && lista.ultimo == nil && lista.largo == 0
+	return lista.primero == nil && lista.ultimo == nil && lista.Largo() == 0
 }
 
 func (lista *listaEnlazada[T]) InsertarPrimero(dato T) {
 	nodo := nodoCrear[T](dato)
-	if lista.primero == nil {
-		//Caso en donde la lista esta vacia
+	if lista.EstaVacia() {
+		// Caso en donde la lista esta vacia
 		lista.primero = nodo
 		lista.primero.prox = lista.ultimo
 		lista.ultimo = nodo
+	} else {
+		nodo.prox = lista.primero
 	}
-	nodo.prox = lista.primero
 	lista.primero = nodo
 	lista.largo++
-
 }
 
 func (lista *listaEnlazada[T]) InsertarUltimo(dato T) {
 	nodo := nodoCrear[T](dato)
-	if lista.primero == nil {
-		//Caso en donde la lista esta vacia
+	if lista.EstaVacia() {
+		// Caso en donde la lista esta vacia
 		lista.primero = nodo
 		lista.primero.prox = lista.ultimo
 		lista.ultimo = nodo
@@ -58,6 +59,7 @@ func (lista *listaEnlazada[T]) InsertarUltimo(dato T) {
 func (lista *listaEnlazada[T]) BorrarPrimero() T {
 	lista.verSiEstaVacia()
 	dato := lista.primero.dato
+	// Caso en el que la lista tiene un solo elemento
 	if lista.primero == lista.ultimo {
 		lista.primero = nil
 		lista.ultimo = nil
@@ -110,19 +112,19 @@ func (lista *listaEnlazada[T]) Iterador() IteradorLista[T] {
 
 // Permite aplicar la funcion a todos los elementos de la lista, teniendo dos condiciones de corte, que no haya mas elementos en la lista o que se devuelva falso.
 func (lista *listaEnlazada[T]) Iterar(visitar func(T) bool) {
-	iter := lista.Iterador()
-	for iter.HaySiguiente() {
-		valor := iter.VerActual()
+	actual := lista.primero
+	for actual != nil {
+		valor := actual.dato
 		if !visitar(valor) {
 			break
 		}
-		iter.Siguiente()
+		actual = actual.prox
 	}
 }
 
 // Esta funcion devuelve el dato en el que esta posicionado el iterador en esa la iteracion actual
 func (iter *iterListaEnlazada[T]) VerActual() T {
-	if iter.actual == nil {
+	if iter.HaySiguiente() {
 		panic("El iterador termino de iterar")
 	} else {
 		return iter.actual.dato
@@ -136,7 +138,7 @@ func (iter *iterListaEnlazada[T]) HaySiguiente() bool {
 
 // Esta funcion, siempre y cuando exista, "avanza" al siguiente nodo
 func (iter *iterListaEnlazada[T]) Siguiente() {
-	if iter.actual == nil {
+	if iter.HaySiguiente() {
 		panic("El iterador termino de iterar")
 	} else {
 		iter.anterior = iter.actual
@@ -145,62 +147,58 @@ func (iter *iterListaEnlazada[T]) Siguiente() {
 }
 
 // Esta funcion se encarga de insertar un elemento en la posicion acutual del iterador
+
 func (iter *iterListaEnlazada[T]) Insertar(elemento T) {
 	elem := nodoCrear[T](elemento)
-	if iter.lista.EstaVacia() {
-		//Caso en que esta vacia
+	if iter.anterior == nil {
+		if iter.actual != nil {
+			elem.prox = iter.actual
+		} else {
+			iter.lista.ultimo = elem
+		}
 		iter.lista.primero = elem
-		iter.lista.ultimo = elem
-	} else if iter.anterior == nil {
-		//Caso donde se inserta primero
-		elem.prox = iter.actual
-		iter.lista.primero = elem
-		iter.actual = elem
-
-	} else if iter.actual == nil {
-		//Caso donde se quiera insertar al ultimo
-		iter.anterior.prox = elem
-		elem.prox = nil
-		iter.actual = elem
-		iter.lista.ultimo = elem
-	} else {
-		iter.anterior.prox = elem
-		iter.lista.ultimo = elem
 	}
-	iter.lista.largo++
+	if iter.anterior != nil {
+		if iter.actual == nil {
+			iter.lista.ultimo = elem
+		} else {
+			elem.prox = iter.actual
+		}
+		iter.anterior.prox = elem
+	}
 	iter.actual = elem
+	iter.lista.largo++
 }
 
+/*Hay errores importantes en el codigo:
+Código repetido: Se debe reutilizar EstaVacia en InsertarPrimero
+Código repetido: Se debe reutilizar EstaVacia en InsertarUltimo
+Advertencia (correccion no obligatoria, pero importante): Se debería reutilizar HaySiguiente en VerActual
+Advertencia (correccion no obligatoria, pero importante): Se debería reutilizar HaySiguiente en Siguiente
+Error: El iteardor interno no debe depender del iterador externo! */
 // Esta funcion se encarga de borrar un elemento en la posicion acutual del iterador
 func (iter *iterListaEnlazada[T]) Borrar() T {
-	// Terminar de chequear esta funcion integra
-	if iter.actual == nil {
+	if !iter.HaySiguiente() {
 		panic("El iterador termino de iterar")
 	}
-	elemento := iter.actual.dato
-
-	if iter.anterior == nil {
-		if iter.actual.prox == nil {
-			iter.actual = nil
-			iter.lista.primero = nil
-			iter.lista.ultimo = nil
-		} else {
-			//Caso de que se elimina el primero
-			iter.lista.primero = iter.actual.prox
-			iter.lista.ultimo = iter.lista.primero
-			iter.actual = iter.actual.prox
-		}
-	} else if iter.actual.prox == nil {
-		//Caso en que se quiera eliminar el ultimo
-		iter.anterior.prox = iter.actual.prox
-		iter.lista.ultimo = iter.anterior
-		iter.actual = nil
-	} else {
-		// Caso general, elementos en el "medio" de la lista
-		iter.actual = iter.actual.prox
-		iter.anterior.prox = iter.actual
+	elemento := iter.VerActual()
+	// Caso en que quede un solo elemento
+	if iter.lista.largo == 0 {
+		iter.lista.primero = nil
+		iter.lista.ultimo = nil
 	}
+	// Caso de que se quiera borrar el primero de la lista
+	if iter.anterior == nil && iter.lista.largo != 0 {
+		iter.lista.primero = iter.actual.prox
+
+	} else {
+		iter.anterior.prox = iter.actual.prox
+	}
+	// Caso de que se quiera borra el ultimo de la lista
+	if iter.actual.prox == nil && iter.lista.largo != 0 {
+		iter.lista.ultimo = iter.anterior
+	}
+	iter.actual = iter.actual.prox
 	iter.lista.largo--
 	return elemento
-
 }
